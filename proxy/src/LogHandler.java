98ceaeac75132211a707e911308e309048a624eb
deadking01 <wuqingfeng234@163.com>
2016-12-12 15:21:28 +0800

    动态代理

---
+++ proxy/src/LogHandler.java
@@ -0,0 +1,24 @@
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * Created by sdzn-dsj on 2016/12/12.
+ */
+public class LogHandler implements InvocationHandler {
+    Object object;
+    LogHandler(Object object){
+        this.object=object;
+    }
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        this.doBefore();
+        Object o=method.invoke(object,args);
+        this.doAfter();
+        return o;
+    }
+    public void doBefore(){
+        System.out.println("do this before");
+    }
+    public void doAfter(){
+        System.out.println("do this after");
+    }
+}
--- threadpool/src/TThread.java
+++ threadpool/src/TThread.java
@@ -19,6 +19,8 @@ public class TThread {
                 }
             };
             thread.start();
+
+
             try{
                 thread.join();
             } catch (InterruptedException e) {
