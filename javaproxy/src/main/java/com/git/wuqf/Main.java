18e70cd3f3299ef6b06d3b82cdd999d3bad271b6
wuqingfeng <wuqingfeng234@163.com>
2018-11-21 21:00:29 +0800

    java动态代理

---
+++ javaproxy/src/main/java/com/git/wuqf/Main.java
@@ -0,0 +1,16 @@
+package com.git.wuqf;
+
+import java.lang.reflect.Proxy;
+
+/**
+ * @author wuqf
+ * @date 2018/11/21
+ */
+public class Main {
+    public static void main(String[] args) {
+        HelloImpl hello = new HelloImpl();
+        DynamicProxy dynamicProxy = new DynamicProxy(hello);
+        Hello o = (Hello) Proxy.newProxyInstance(HelloImpl.class.getClassLoader(), HelloImpl.class.getInterfaces(), dynamicProxy);
+        o.sayHello("xx");
+    }
+}
