32b9fbf89dc7df7a70d331d16b2b6d3d79af45e3
wuqingfeng <wuqingfeng234@163.com>
2019-10-29 09:50:40 +0800

    stream

--- stream/src/main/java/com/huawei/wuqf/java/Main.java
+++ stream/src/main/java/com/huawei/wuqf/java/Main.java
@@ -4,6 +4,7 @@ package com.huawei.wuqf.java;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -11,6 +12,10 @@ import java.util.stream.Stream;
  */
 public class Main {
     public static void main(String[] args) {
+        word();
+    }
+
+    public static void stream() {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         Stream<Integer> stream = numbers.stream();
         stream.filter((x) -> {
@@ -20,5 +25,19 @@ public class Main {
         }).forEach(System.out::println);
     }
 
+    public static void word() {
+        String ss = "Hello";
+        String[] aa = ss.split("");
+        String[] bb = {"H", "e", "l", "l", "o"};
+        String[] strings = {"Hello", "World"};
+
+        Stream<String[]> stream1 = Arrays.asList(strings).stream().
+                map(str -> str.split(""));
+        Stream<String> stringStream = stream1.flatMap(strings1 -> Arrays.stream(strings1));
+
+
+        List<String> ls = stringStream.collect(Collectors.toList());
+        System.out.println("xxx");
+    }
 }
 
